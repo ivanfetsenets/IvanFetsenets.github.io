3088417d027b39e7757e1a33d35f5eb64e0f3098
Ivan <ivan.fetsenets@gmail.com>
2016-06-09 17:01:20 +0300

    09-06-2016 commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,74 @@
 /* кнопка вверх */
-$(document).ready(function(){
+$(document).ready(function()
+{
+ 	$(window).scroll(function()
+ 		{
+			if ($(this).scrollTop() > 10) 
+			{
+				$('.scrollup').fadeIn();
+			} 
+
+			else 
+			{
+				$('.scrollup').fadeOut();
+			}
+		});
  
-$(window).scroll(function(){
-if ($(this).scrollTop() > 10) {
-$('.scrollup').fadeIn();
-} else {
-$('.scrollup').fadeOut();
-}
-});
- 
-$('.scrollup').click(function(){
-$("html, body").animate({ scrollTop: 0 }, 600);
-return false;
+$('.scrollup').click(function()
+{
+	$("html, body").animate({ scrollTop: 0 }, 600);
+	return false;
 });
  
 });
 /* кнопка вверх */
+
+	/* Weather forecast*/
+
+	function request(city_id) 
+	{
+		// some needed parameters
+		var content = document.getElementById("content");
+		var my_wind = document.getElementById("wind");
+		var my_clouds = document.getElementById("clouds");
+		var my_pressure = document.getElementById("pressure");
+
+		var URL = "http://api.openweathermap.org/data/2.5/weather?id="; 
+		var API_KEY = "d75da5829ee2244d5a25bbb10c1a7318";
+
+		var REQ_URL = URL + city_id + "&appid=" + API_KEY;
+
+
+		var XHR = new XMLHttpRequest();
+		XHR.open("GET", REQ_URL, true);
+		XHR.send();
+
+		// Hadling Server's response
+		XHR.onreadystatechange = function() 
+		{
+			if ((XHR.readyState == 4) && (XHR.status == 200)) 
+			{
+				RESP = JSON.parse(XHR.responseText);
+
+				// Generating DOM
+				content.innerHTML = "";
+				my_wind.innerHTML = "";
+				my_clouds.innerHTML = "";
+				my_pressure.innerHTML = "";								
+				/*var elem = document.createElement("div");*/
+				content.innerHTML = RESP.name;
+				my_wind.innerHTML = Resp.wind;
+				
+				content.appendChild(elem);
+			}
+		}
+	}
+
+	/*var cities = document.getElementById("mycity");
+
+	cities.addEventListener("change", function(){
+		console.log(cities.value);
+		request(cities.value);
+	}, false); */
+
+	/* End weather forecast*/
